837ca425ac66d43bf534ba55ba340e7a630585e3
ilya5006 <samarcev.ilya@mail.ru>
2020-01-04 22:10:53 +0300

    Other skills

--- skills.php
+++ skills.php
@@ -3,4 +3,6 @@
     <p class="skill">Клиентский язык - JavaScript</p>
     <p class="skill">СУБД - MySQL</p>
     <p class="skill">Вёрстка - HTML 5 + CSS 3</p>
+    <p class="skill">Система контроля версий - GIT</p>
+    <p class="skill">Git-клиент - GitKraken</p>
 </div>
