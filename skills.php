c60fbad60c9200e26a6781be18c3fb8e3b8d6e65
ilya5006 <samarcev.ilya@mail.ru>
2020-01-04 21:13:47 +0300

    Skills page

--- skills.php
+++ skills.php
@@ -1,3 +1,6 @@
 <div id="skills_page" class="hide_page">
-    
+    <p class="skill">Серверный язык - PHP 7</p>
+    <p class="skill">Клиентский язык - JavaScript</p>
+    <p class="skill">СУБД - MySQL</p>
+    <p class="skill">Вёрстка - HTML 5 + CSS 3</p>
 </div>
